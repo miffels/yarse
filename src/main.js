85d9f8cab7ac79752f7a2e6214eaf4eb637bbcfb
Michael Jess <mijess@mail.uni-mannheim.de>
2012-10-25 00:45:39 +0200

    Tested code for getting specific recipe info

--- src/main.js
+++ src/main.js
@@ -1,11 +1,33 @@
 'use strict';
 var FatSecretRequestBlank = require('../src/FatSecretRequestBlank');
+var $ = require('jQuery')
 
 var requestBlank = new FatSecretRequestBlank();
 
-requestBlank.parameters.method = 'foods.search';
+requestBlank.parameters.format = 'json';
+requestBlank.parameters.method = 'recipes.search';
 requestBlank.parameters.search_expression = 'potato';
 
-requestBlank.makeRequest(function(request) {
-	console.log(request);
+function resolve(url, done) {
+	$.ajax({
+		url: url
+	}).done(function(result) {
+		done(result);
+	}).error(function(error) {
+		console.log(error.responseText);
+	});
+}
+
+requestBlank.makeRequest(function(url) {
+	resolve(url, function(result) {
+		var recipeBlank = new FatSecretRequestBlank();
+		recipeBlank.parameters.format = 'json';
+		recipeBlank.parameters.method = 'recipe.get';
+		recipeBlank.parameters.recipe_id = result.recipes.recipe[0].recipe_id;
+		recipeBlank.makeRequest(function (recipeUrl) {
+			resolve(recipeUrl, function(recipe) {
+			console.log(recipe);
+		});
+		});
+	});
 });

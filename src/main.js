045abc0143e509745fadf506d0a940124ebec23d
Michael Jess <michael.jess@sap.com>
2012-10-26 10:59:20 +0200

    Merging with origin

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 'use strict';
-var Recipe = require('recipe');
+var $ = require('jquery');
 
-var recipe = new Recipe("Stuff");
-console.log(recipe);
+console.log('Querying Google.')
+$.ajax({url:'http://www.google.de/'})
+	.done(function(data) {
+		console.log('Done.');
+		console.log(data);
+	})
+	.always(function() {
+		console.log('In always');
+	})
+	.fail(function(error) {
+		console.log('Failure.');
+		console.log(error.responseText);
+	});
